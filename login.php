b617c4cda873b5a5d579825423b44611b0398cdb
SingularD <906229740@qq.com>
2017-05-31 15:19:20 +0800

    2017.5.31

--- login.php
+++ login.php
@@ -35,19 +35,41 @@ if (isset($_REQUEST['act'])){
     }
 }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>首页</title>
+    <link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap.min.css" type="text/css">
+    <link href="./css/style.css" rel="stylesheet" type="text/css">
+    <script src="jquery-3.2.1.min.js"></script>
+    <script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<form action="login.php" method="post">
-    用户：<input type="text" name="username"><br>
-    密码：<input type="password" name="password"><br>
-    <input type="submit">
-</form>
+<div class="container">
+    <div class="header">
+        <div class="home"><a href=""> login</a></div>
+        <div class="form">
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for="exampleInputText">Username</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Username"
+                           name="username">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"
+                           name="password">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+
+            </form>
+        </div>
+        <div class="welcome">WELCOME TO MY WEB</div>
+        <div class="welcome-word">It's LiSongWei's Web page</div>
+    </div>
+    <div class="bodyer"></div>
+    <div class="footer"></div>
+</div>
 </body>
 </html>
